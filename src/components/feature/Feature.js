9ec8e5bce913f8d4504ea1cccf7c430a5c06c113
Abiodun <abiodunhodonu@gmail.com>
2022-07-08 23:21:43 +0100

    made feature section responsive

--- src/components/feature/Feature.js
+++ src/components/feature/Feature.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import '../feature/feature.css'
 
-const Feature = () => {
+const Feature = ({title, text}) => {
   return (
-    <div>
-      Featrues
+    <div className='barry__features-container__feature'>
+      <div className='barry__features-container__feature-title'>
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className='barry__features-container_feature-text'>
+        <p>{text}</p>
+      </div>
     </div>
   )
 }
--- src/containers/WhatBarry/WhatBarry.js
+++ src/containers/WhatBarry/WhatBarry.js
@@ -4,9 +4,18 @@ import '../WhatBarry/barry.css'
 
 const WhatBarry = () => {
   return (
-    <div className='barry__whatbarry section__margin'>
-      <div className='barry__whatbarry-features'>
-        <Feature />
+    <div className='barry__whatbarry section__margin' id="wbr">
+      <div className='barry__whatbarry-feature'>
+        <Feature title = "What is Barry Ai?" text ="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."/>
+      </div>
+      <div className='barry__whatbarry-heading'>
+        <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className='barry__whatbarry-container'>
+        <Feature title='Chatbots' text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. "/>
+        <Feature title='Knowledgebase' text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"/>
+        <Feature title='Education' text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"/>
 
       </div>
     </div>
