2ee83f96ed7d1ce1797e8aa512a920a96cf8d54c
Abiodun <abiodunhodonu@gmail.com>
2022-07-09 11:38:43 +0100

    added the cta section

--- src/components/cta/Cta.js
+++ src/components/cta/Cta.js
@@ -3,8 +3,14 @@ import '../cta/cta.css'
 
 const Cta = () => {
   return (
-    <div>
-      CTA
+    <div className='barry__cta'>
+      <div className='barry__cta-content'>
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today &amp; start exploring the endless possiblities.</h3>
+      </div>
+      <div className='barry__cta-btn'>
+        <button type='button'>Get Started</button>
+      </div>
     </div>
   )
 }
